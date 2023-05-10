215e6fd33a5a743db75940ac9eda79f5069ad7cc
xxxasdfghjk <xxxasdfhjk999@gmail.com>
2023-05-11 02:31:16 +0900

    learned chapter 5

---
+++ chapter5/src/bin/tuple_struct.rs
@@ -0,0 +1,9 @@
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn main() {
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    let mut temp: Color = Color { ..black };
+    println!("{}", black.0);
+}
